1a38f1ee137b6c9b7f30808c2e8fabccc018412e
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2023-05-17 17:44:41 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+import styled from 'styled-components';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/PQouAC2yqYyzND39/scene.splinecode" />
+  );
+}
